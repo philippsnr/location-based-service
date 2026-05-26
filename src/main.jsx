75989d57fdd1da6ce89898288eb4628b132dfea4
Niklas Müller <niklasmueller.785@gmail.com>
2026-05-26 14:42:12 +0200

    feat: install and configure leaflet-routing-machine (#17)
    Add leaflet-routing-machine dependency and a RoutingMachine wrapper
    component that attaches an L.Routing.Control to the parent MapContainer
    via the react-leaflet useMap hook. Imports leaflet and routing-machine
    CSS in main.jsx so styles are available once a MapContainer is mounted.

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import Framework7 from 'framework7/lite-bundle';
 import Framework7React from 'framework7-react';
 import 'framework7/css/bundle';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import App from './App.jsx';
 
 Framework7.use(Framework7React);
