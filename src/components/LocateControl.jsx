9c815600f8ed772e2ba7e3a1d0c72e7402e92cb6
Niklas Müller <niklasmueller.785@gmail.com>
2026-05-28 08:56:55 +0200

    feat: use location pin icon for Locate Me button (#11)

--- src/components/LocateControl.jsx
+++ src/components/LocateControl.jsx
@@ -14,7 +14,10 @@ export default function LocateControl({ onLocate }) {
       btn.type = 'button'
       btn.title = 'Locate me'
       btn.setAttribute('aria-label', 'Locate me')
-      btn.innerHTML = '◎'
+      btn.innerHTML =
+        '<svg viewBox="0 0 24 24" width="18" height="18" aria-hidden="true" focusable="false">' +
+        '<path fill="currentColor" d="M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"/>' +
+        '</svg>'
       L.DomEvent.disableClickPropagation(container)
       L.DomEvent.on(btn, 'click', (e) => {
         L.DomEvent.stop(e)
