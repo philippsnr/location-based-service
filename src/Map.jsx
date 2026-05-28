d873d502b99ba7d88920c7c8bbf26eff8bd29655
DominikKien <kienle.dominik.03@gmail.com>
2026-05-28 08:36:37 +0200

    Implemented
    Marker follows the mouse click on the map

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,22 +1,36 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
-const position = [54.4047, 10.2256];
-const marker = [54.4047, 10.2256];
+const initialPosition = [54.4047, 10.2256];
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+
+  useMapEvents({
+    click(e) {
+      setPosition(e.latlng);
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>
+        Name of the place <br /> LABOE!!!!!
+      </Popup>
+    </Marker>
+  );
+}
 
 function Map() {
   return (
-    <MapContainer center={position} zoom={15} scrollWheelZoom={true} className="map-container">
+    <MapContainer center={initialPosition} zoom={15} scrollWheelZoom={true} className="map-container">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={marker}>
-        <Popup>
-          Name of the place <br /> LABOE!!!!!
-        </Popup>
-      </Marker>
+      <LocationMarker />
     </MapContainer>
   );
 }
